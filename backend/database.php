e4b675bd5f4418f15b9b2f38961acbe424132c47
Dorian DESMARS <dorian.desmars@isen-ouest.yncrea.fr>
2026-03-23 16:46:21 +0100

    database connection

---
+++ backend/database.php
@@ -0,0 +1,10 @@
+<?php
+$db_file = __DIR__ . '/db/backabase.db';
+
+try {
+    $pdo = new PDO("sqlite:" . $db_file);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
